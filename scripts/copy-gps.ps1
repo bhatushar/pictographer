d5b59e005265097f3b743a73f2f8552baded1ab2
Tushar Bhatt <tbhatt321@gmail.com>
2023-01-27 14:35:39 +0530

    Added .env file

--- scripts/copy-gps.ps1
+++ scripts/copy-gps.ps1
@@ -1,5 +1,11 @@
 # Requries at least 2 arguments: First is the source path, rest are destination paths
 
+# Load env variables
+Get-Content .env | ForEach-Object {
+    $key, $value = $_.split("=")
+    Set-Content Env:\$key $value
+}
+
 $src, $targets = $args
 # Don't use -GPS:All because that will copy GPS Datetime as well and it might clash is the original datetime of the targets
-.\exiftool.exe -TagsFromFile $src -GPSLatitude -GPSLatitudeRef -GPSLongitude -GPSLongitudeRef -GPSAltitude -GPSAltitudeRef -Overwrite_Original $targets
+& $Env:Exiftool_Path -TagsFromFile $src -GPSLatitude -GPSLatitudeRef -GPSLongitude -GPSLongitudeRef -GPSAltitude -GPSAltitudeRef -Overwrite_Original $targets
--- scripts/export-exif.ps1
+++ scripts/export-exif.ps1
@@ -1,8 +1,14 @@
 # Requires 1 command-line argument: Path to the directory containing the media
 
+# Load env variables
+Get-Content .env | ForEach-Object {
+    $key, $value = $_.split("=")
+    Set-Content Env:\$key $value
+}
+
 # DateTimeOriginal is the date on which the photo was taken
 # TrackCreateDate is the date on which the video was recorded, it excludes the timezone
 # XpKeywords are photo tags (Separated by semi-colon)
 # Category are video tags (Separated by comma)
 
-.\exiftool.exe -n -r -FileName -DateTimeOriginal -TrackCreateDate -XpKeywords -Category -Title -GPSLatitude -GPSLatitudeRef -GPSLongitude -GPSLongitudeRef -csv > metadata_raw.csv "$($args[0])"
+& $Env:Exiftool_Path -n -r -csv > $Env:Raw_Metadata_Path -FileName -DateTimeOriginal -TrackCreateDate -XpKeywords -Category -Title -GPSLatitude -GPSLatitudeRef -GPSLongitude -GPSLongitudeRef "$($args[0])"
